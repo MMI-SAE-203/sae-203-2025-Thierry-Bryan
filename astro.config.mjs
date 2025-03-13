76e38c31a0f4cb1494eaf00442c5270bd720e570
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2025-03-13 16:28:20 +0100

    Add Netlify configuration and update dependencies; enhance 'À propos' page with new content sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   experimental: {
     svg: true
   },
+
   output: "server",
+  adapter: netlify(),
 });
