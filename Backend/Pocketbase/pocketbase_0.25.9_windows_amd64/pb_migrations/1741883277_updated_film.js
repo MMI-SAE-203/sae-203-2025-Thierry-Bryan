adcf1f49b91632717c28ce5e9abdd1b6190a23b8
Thierry-Bryan <bryan.thierry@edu.univ-fcomte.fr>
2025-03-13 20:54:10 +0100

    Add favorite functionality to film cards; update README and database

---
+++ Backend/Pocketbase/pocketbase_0.25.9_windows_amd64/pb_migrations/1741883277_updated_film.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2773080466")
+
+  // add field
+  collection.fields.addAt(13, new Field({
+    "hidden": false,
+    "id": "bool834872389",
+    "name": "favoris_film",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2773080466")
+
+  // remove field
+  collection.fields.removeById("bool834872389")
+
+  return app.save(collection)
+})
